d0827d112ecb074867c9c02652df9977ac1934f6
Henjo Hoeksma <henjo@upassist.com>
2021-04-02 16:55:53 +0200

    BUGFIX: Wrong provider could potentially be loaded when running multiple apps with the same provider

--- Classes/OAuthClients/AbstractClient.php
+++ Classes/OAuthClients/AbstractClient.php
@@ -1,11 +1,18 @@
 <?php
+
 namespace CloudTomatoes\OAuth2\OAuthClients;
 
+use CloudTomatoes\OAuth2\Domain\Repository\AppRepository;
+use Flownative\OAuth2\Client\Authorization;
 use Flownative\OAuth2\Client\OAuthClient;
+use Flownative\OAuth2\Client\OAuthClientException;
+use GuzzleHttp\Psr7\Uri;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Neos\Flow\ObjectManagement\ObjectManagerInterface;
 use CloudTomatoes\OAuth2\Domain\Model\App;
 use Neos\Flow\Annotations as Flow;
 use CloudTomatoes\OAuth2\Domain\Repository\ProviderRepository;
+use Psr\Http\Message\UriInterface;
 
 abstract class AbstractClient extends OAuthClient
 {
@@ -28,6 +35,9 @@ abstract class AbstractClient extends OAuthClient
     {
         if ($app instanceof App) {
             $this->app = $app;
+        } elseif (is_string($app)) {
+            $appRepository = new AppRepository();
+            $this->app = $appRepository->findOneByName($app);
         }
         $serviceName = $this->app ? $this->app->getName() : $app;
         parent::__construct($serviceName);
@@ -91,7 +101,7 @@ abstract class AbstractClient extends OAuthClient
             $providerRepository = $this->objectManager->get(ProviderRepository::class);
             $provider = $providerRepository->findOneByOauthClient('CloudTomatoes\OAuth2\OAuthClients\AzureClient');
         }
-        return trim($provider->getAuthenticationEndpoint(), '/')  . '/token/resource';
+        return trim($provider->getAuthenticationEndpoint(), '/') . '/token/resource';
     }
 
     /**
@@ -102,5 +112,58 @@ abstract class AbstractClient extends OAuthClient
     public function getClientId(): string
     {
         return $this->app->getClientId();
+    }
+
+    /**
+     * Finish an OAuth authorization with the Authorization Code flow
+     *
+     * @param string $stateIdentifier The state identifier, passed back by the OAuth server as the "state" parameter
+     * @param string $code The authorization code given by the OAuth server
+     * @param string $scope The scope granted by the OAuth server
+     * @return UriInterface The URI to return to
+     * @throws OAuthClientException
+     */
+    public function finishAuthorization(string $stateIdentifier, string $code, string $scope): UriInterface
+    {
+        $stateFromCache = $this->stateCache->get($stateIdentifier);
+        if (empty($stateFromCache)) {
+            throw new OAuthClientException(sprintf('OAuth: Finishing authorization failed because oAuth state %s could not be retrieved from the state cache.', $stateIdentifier), 1558956494);
+        }
+
+        $authorizationId = $stateFromCache['authorizationId'];
+        $clientId = $stateFromCache['clientId'];
+        $clientSecret = $stateFromCache['clientSecret'];
+        $oAuthProvider = $this->createOAuthProvider($clientId, $clientSecret);
+
+        $this->logger->info(sprintf('OAuth (%s): Finishing authorization for client "%s", authorization id "%s", using state %s.', $this->getServiceType(), $clientId, $authorizationId, $stateIdentifier));
+        try {
+            $authorization = $this->entityManager->find(Authorization::class, $authorizationId);
+
+            if (!$authorization instanceof Authorization) {
+                throw new OAuthClientException(sprintf('OAuth2 (%s): Finishing authorization failed because authorization %s could not be retrieved from the database.', $this->getServiceType(), $authorizationId), 1568710771);
+            }
+
+            $this->logger->debug(sprintf('OAuth (%s): Retrieving an OAuth access token for authorization "%s" in exchange for the code %s', $this->getServiceType(), $authorizationId, str_repeat('*', strlen($code) - 3) . substr($code, -3, 3)));
+            $accessToken = $oAuthProvider->getAccessToken(Authorization::GRANT_AUTHORIZATION_CODE, ['code' => $code]);
+
+            $this->logger->info(sprintf('OAuth (%s): Persisting OAuth token for authorization "%s" with expiry time %s.', $this->getServiceType(), $authorizationId, $accessToken->getExpires()));
+
+            $authorization->setAccessToken($accessToken);
+
+            $accessTokenValues = $accessToken->getValues();
+            $scope = $accessTokenValues['scope'] ?? $scope;
+            $authorization->setScope($scope);
+            $this->entityManager->persist($authorization);
+            $this->entityManager->flush();
+
+        } catch (IdentityProviderException $exception) {
+            throw new OAuthClientException($exception->getMessage() . ' ' . $exception->getResponseBody()['error_description'], 1511187001671, $exception);
+        }
+
+        $returnToUri = new Uri($stateFromCache['returnToUri']);
+        $returnToUri = $returnToUri->withQuery(trim($returnToUri->getQuery() . '&' . self::AUTHORIZATION_ID_QUERY_PARAMETER_NAME . '=' . $authorizationId, '&'));
+
+        $this->logger->debug(sprintf('OAuth (%s): Finished authorization "%s", $returnToUri is %s.', $this->getServiceType(), $authorizationId, $returnToUri));
+        return $returnToUri;
     }
 }
