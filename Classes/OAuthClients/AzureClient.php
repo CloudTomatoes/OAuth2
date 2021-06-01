fdf9c59d76cc30456c89b237bd514005a3e1a361
Henjo Hoeksma <henjo@upassist.com>
2021-06-01 14:06:08 +0200

    TASK: Apply migration Neos.Flow-20201207104500
    Adjust code to deprecation of ComponentContext/ComponentParameters

--- Classes/OAuthClients/AzureClient.php
+++ Classes/OAuthClients/AzureClient.php
@@ -35,7 +35,7 @@ class AzureClient extends AbstractClient
     {
         $currentRequestHandler = $this->bootstrap->getActiveRequestHandler();
         if ($currentRequestHandler instanceof HttpRequestHandlerInterface) {
-            $httpRequest = $currentRequestHandler->getComponentContext()->getHttpRequest();
+            $httpRequest = $currentRequestHandler->getHttpRequest();
         } else {
             putenv('FLOW_REWRITEURLS=1');
             $httpRequest = $this->serverRequestFactory->createServerRequest('GET', new Uri($this->flowBaseUriSetting));
